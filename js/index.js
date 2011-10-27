d9b149a5bd0b8a2346d26766c839d87711ff18d7
Matt Glazar <matt.glazar@gmail.com>
2011-10-26 18:32:04 -0700

    ?auto query + run button

--- js/index.js
+++ js/index.js
@@ -12,8 +12,50 @@ define([ 'tests/performance', 'testDom', 'testRunner' ], function (performance, 
         // Do nothing
     }
 
-    testRunner.run('performance', performance, {
-        done: allTestsDone,
-        step: testDone
+    registerOnLoad(function () {
+        var performanceTestsRunning = false;
+
+        var runPerformanceTestsButton = document.getElementById('start-performance-tests');
+        runPerformanceTestsButton.disabled = false;
+
+        function runPerformanceTests() {
+            if (performanceTestsRunning) {
+                return;
+            }
+
+            performanceTestsRunning = true;
+            runPerformanceTestsButton.disabled = true;
+
+            testRunner.run('performance', performance, {
+                done: function (err, results) {
+                    performanceTestsRunning = false;
+                    runPerformanceTestsButton.disabled = false;
+
+                    allTestsDone(err, results);
+                },
+                step: testDone
+            });
+        }
+
+        runPerformanceTestsButton.addEventListener('click', function () {
+            runPerformanceTests();
+        }, false);
+
+        var getVars = { };
+        if (/^\?/.test(location.search)) {
+            location.search.substr(1).split(/&/g).forEach(function (part) {
+                var equalsIndex = part.indexOf('=');
+
+                if (equalsIndex >= 0) {
+                    getVars[part.substr(0, equalsIndex)] = part.substr(equalsIndex + 1);
+                } else {
+                    getVars[part] = true;
+                }
+            });
+        }
+
+        if ('auto' in getVars) {
+            runPerformanceTests();
+        }
     });
 });
