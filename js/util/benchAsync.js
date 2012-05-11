b2281c9c3c65b9c31bd4a8b53630da26bd0cefe4
Ben Savage <ben@sibblingz.com>
2012-05-11 12:22:12 -0700

    improving the algorithm used to hone in on the correct value and using request animation frame to get a more accurate FPS count

--- js/util/benchAsync.js
+++ js/util/benchAsync.js
@@ -8,13 +8,33 @@ define([ 'util/ensureCallback' ], function (ensureCallback) {
 
         var operationCount = 0;
         var startTime;
+        
+        // var raf = window.requestAnimationFrame
+        //     || window.webkitRequestAnimationFrame
+        //     || window.mozRequestAnimationFrame
+        //     || window.oRequestAnimationFrame
+        //     || window.msRequestAnimationFrame;
+        // raf(rafUpdate);
+        // 
+        // var rafTicks = 0;
+        // function rafUpdate() {
+        //     ++rafTicks;
+        //     raf(rafUpdate);
+        // }
 
+        var rafTicks = 0;
+        window.addEventListener('touchmove', function (event) {
+            ++rafTicks;
+            event.preventDefault();
+        }, false);
+        
         function next() {
             fn(operationCount, function () {
                 ++operationCount;
 
                 var endTime = Date.now();
                 if (endTime - startTime >= maxTime) {
+                    console.log(rafTicks);
                     var elapsed = endTime - startTime;
                     var score = operationCount / elapsed * maxTime;
                     return callback(null, score, elapsed);
