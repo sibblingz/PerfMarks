0b65c51a28faafccfb3de35fcd614c97028fb281
Ben Savage <ben@sibblingz.com>
2012-05-11 18:46:34 -0700

    Add webGLBatchDraw to list of renderers

--- js/sprites/renderers.js
+++ js/sprites/renderers.js
@@ -11,7 +11,8 @@
         'canvasDrawImagePartialClear',
         'canvasDrawImagePartialClearAlign',
 
-        'webGLDrawWithUniform'
+        'webGLDrawWithUniform',
+        'webGLBatchDraw'
     ];
 
     var filenames = [ ];
